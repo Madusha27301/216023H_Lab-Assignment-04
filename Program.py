6d09c00778c42c21a614ec55913047008a6f7449
sasi1219 <sasinidr@gmail.com>
2024-05-21 15:08:11 +0530

    Update Program.py

--- Program.py
+++ Program.py
@@ -7,7 +7,9 @@ print(value)
 person_1 = Person("Kasun",19)
 person_2 = Person("Supun",35)
 person_3 = Person("Nimal",28)
-
+person_4 = Person("Chathu",21)
+person_5 = Person("Sanka", 25)
 
 print(person_1.age)
 print(person_2.name)
+print(person_5.age)
